d35e954f059035605af4c16720a830a4aae7f5b3
Abi Noda <a@abinoda.com>
2013-08-27 09:04:33 -0500

    tests for initialize, transactions, acct_number added

--- account_spec.rb
+++ account_spec.rb
@@ -3,24 +3,39 @@ require "rspec"
 require_relative "account"
 
 describe Account do
-  describe "#initialize" do
+  let(:account) { Account.new("4532145896", 10000)}
 
+  describe "#initialize" do
+    it "creates an Account object" do
+      account.should be_an_instance_of Account
+    end
   end
 
   describe "#transactions" do
+    it "should have transactions" do
+      account.transactions.should be_true
+    end
 
+    it "should be equal to starting_balance" do
+      account.transactions.should start_with [10000]
+    end
   end
 
   describe "#balance" do
-
+    
   end
 
   describe "#account_number" do
-
+    it "should hide first 6 digits" do
+      account.acct_number.should start_with "******"
+    end
+    it "should show the last 4 digits" do
+      account.acct_number.should end_with "5896"
+    end
   end
 
   describe "deposit!" do
-
+  
   end
 
   describe "#withdraw!" do
